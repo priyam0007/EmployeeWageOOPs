fdb86b009b6f71c3e50ea4cf8df81e45fb98fefb
Priya <Priyamadiwalar0007@gmail.com>
2022-02-12 23:58:59 +0530

    Added: Attendance Check

--- src/com/blz_employeewageoops/EmployeeWageOOPs.java
+++ src/com/blz_employeewageoops/EmployeeWageOOPs.java
@@ -1,5 +1,7 @@
 package com.blz_employeewageoops;
 
+import java.util.Random;
+
 public class EmployeeWageOOPs {
 	public static void main(String[] args) {
 		Attendence attendence = new Attendence();
@@ -10,8 +12,9 @@ class Attendence {
 	public static final int IS_PRESENT = 1;
 	public static final int IS_ABSENT = 0;
 	public void empCheck() {
-		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
-		switch(empCheck) {
+		Random random = new Random();
+		int emp = random.nextInt(2);
+		switch(emp) {
 		case IS_PRESENT: 
 			System.out.println("Employee is present");
 			break;
