640bbf575c7c567042f4b8904ea29a391b72088b
Priya <Priyamadiwalar0007@gmail.com>
2022-02-13 00:26:01 +0530

    Added: EmployeeDailyWage

--- src/com/blz_employeewageoops/EmployeeWageOOPs.java
+++ src/com/blz_employeewageoops/EmployeeWageOOPs.java
@@ -3,28 +3,33 @@ package com.blz_employeewageoops;
 import java.util.Random;
 
 public class EmployeeWageOOPs {
+	
 	public static void main(String[] args) {
-		Attendence attendence = new Attendence();
-		attendence.empCheck();
-	}
-}
-class Attendence {
-	public static final int IS_PRESENT = 1;
-	public static final int IS_ABSENT = 0;
-	public void empCheck() {
-		Random random = new Random();
-		int emp = random.nextInt(2);
-		switch(emp) {
-		case IS_PRESENT: 
-			System.out.println("Employee is present");
-			break;
-		case IS_ABSENT:
-			System.out.println("Employee is absent");
-			break;
-		default:
-			System.out.println("Welcome");
+			EmpCalculation emp = new EmpCalculation();
+			emp.empCheck();
+			DailyWage wage = new DailyWage();
+			wage.dailyWageCalculation();
 		}
 	}
-}
-
-
+	class EmpCalculation {
+		public static int empHrs;
+		public void empCheck() {
+			Random random = new Random();
+			int empCheck = random.nextInt(2);
+			if(empCheck == 1) {
+				System.out.println("Employee is present");
+				empHrs = 8;
+			}
+			else{
+				System.out.println("Employee is absent");
+			}
+		}
+	}
+	class DailyWage{
+		public static final int WAGE_PER_HOUR = 20;
+		
+		public void dailyWageCalculation() {
+		int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+		System.out.println("Daily employee wage is "+totalDailyWage);
+		}
+	}
