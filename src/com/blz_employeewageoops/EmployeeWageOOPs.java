443f3accca7b1d9ac218886484b71c5a2dd9166f
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 21:05:38 +0530

    Added: Calculating Wages for a Month

--- src/com/blz_employeewageoops/EmployeeWageOOPs.java
+++ src/com/blz_employeewageoops/EmployeeWageOOPs.java
@@ -3,42 +3,55 @@ package com.blz_employeewageoops;
  import java.util.Random;
 
   public class EmployeeWageOOPs {
-	
-	public static void main(String[] args) {
-		EmpCalculation emp = new EmpCalculation();
-		emp.empCheck();
-		DailyWage wage = new DailyWage();
-		wage.dailyWageCalculation();
-	}
+	  public static int WORKING_DAYS = 20;
+		public static void main(String[] args) {
+			for (int i = 1;  i<= WORKING_DAYS; i++) {
+				System.out.println("For DAY"+i);
+				EmpCalculation emp = new EmpCalculation();
+				emp.empCheck();
+				DailyWage wage = new DailyWage();
+				wage.dailyWageCalculation();
+				wage.totalWage();
+			}
+			System.out.println("Total Wage of employee in a month = "+DailyWage.totalWage);
+		}
 
-}
- class EmpCalculation {
-	public static final int IS_FULLTIME = 1;
-	public static final int IS_PARTTIME = 2;
-	public static int empHrs;
-	public void empCheck() {
-		Random random = new Random();
-		int empCheck = random.nextInt(3);
-		switch(empCheck) {
-		case IS_FULLTIME:
-			System.out.println("Employee is present and worked full-time");
-			empHrs = 8;
-			break;
-		case IS_PARTTIME:
-			System.out.println("Employee is present and worked part-time");
-			empHrs = 4;
-			break;
-			
-		default:
-			System.out.println("Employee is absent");
+	}
+	class EmpCalculation 
+	{
+		public static final int IS_FULLTIME = 1;
+		public static final int IS_PARTTIME = 2;
+		public static int empHrs;
+		public void empCheck() {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch(empCheck) {
+			case IS_FULLTIME:
+				System.out.println("Employee is present and worked full-time");
+				empHrs = 8;
+				break;
+			case IS_PARTTIME:
+				System.out.println("Employee is present and worked part-time");
+				empHrs = 4;
+				break;
+				
+			default:
+				System.out.println("Employee is absent");
+				empHrs = 0;
+			}
 		}
 	}
-}
- class DailyWage{
-	public static final int WAGE_PER_HOUR = 20;
-	
-	public void dailyWageCalculation() {
-	int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
-	System.out.println("Daily employee wage is "+totalDailyWage);
+	class DailyWage{
+		public static final int WAGE_PER_HOUR = 20;
+		static int totalWage = 0;
+		int totalDailyWage;
+		public void dailyWageCalculation() {
+		totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+		System.out.println("Daily employee wage is "+totalDailyWage);
+		}
+		public void totalWage() {
+			totalWage = totalWage + totalDailyWage; 
+		}
 	}
-}
+  
+	
+	
