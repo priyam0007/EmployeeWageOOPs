897f3f02e0ab2a7a41c735a392177c720609e70e
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 20:41:33 +0530

    Added: Add Part time

--- src/com/blz_employeewageoops/EmployeeWageOOPs.java
+++ src/com/blz_employeewageoops/EmployeeWageOOPs.java
@@ -1,35 +1,40 @@
 package com.blz_employeewageoops;
 
-import java.util.Random;
+ import java.util.Random;
 
-public class EmployeeWageOOPs {
+  public class EmployeeWageOOPs {
 	
 	public static void main(String[] args) {
-			EmpCalculation emp = new EmpCalculation();
-			emp.empCheck();
-			DailyWage wage = new DailyWage();
-			wage.dailyWageCalculation();
+		EmpCalculation emp = new EmpCalculation();
+		emp.empCheck();
+		DailyWage wage = new DailyWage();
+		wage.dailyWageCalculation();
+	}
+
+}
+ class EmpCalculation {
+	public static int empHrs;
+	public void empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(3);
+		if(empCheck == 1) {
+			System.out.println("Employee is present and worked full-time");
+			empHrs = 8;
+		}
+		else if(empCheck == 2){
+			System.out.println("Employee is present and worked part-time");
+			empHrs = 4;
+		}
+		else{
+			System.out.println("Employee is absent");
 		}
 	}
-	class EmpCalculation {
-		public static int empHrs;
-		public void empCheck() {
-			Random random = new Random();
-			int empCheck = random.nextInt(2);
-			if(empCheck == 1) {
-				System.out.println("Employee is present");
-				empHrs = 8;
-			}
-			else{
-				System.out.println("Employee is absent");
-			}
-		}
+}
+ class DailyWage{
+	public static final int WAGE_PER_HOUR = 20;
+	
+	public void dailyWageCalculation() {
+	int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+	System.out.println("Daily employee wage is "+totalDailyWage);
 	}
-	class DailyWage{
-		public static final int WAGE_PER_HOUR = 20;
-		
-		public void dailyWageCalculation() {
-		int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
-		System.out.println("Daily employee wage is "+totalDailyWage);
-		}
-	}
+}
